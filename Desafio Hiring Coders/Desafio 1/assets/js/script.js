744f2d99d971edc9cf5aa076b73d001d13c9e9ea
mariaeduardaxs <m_eduardaxavier@hotmail.com>
2021-07-19 03:21:05 -0300

    mudança de pastas

--- Desafio Hiring Coders/Desafio 1/assets/js/script.js
+++ Desafio Hiring Coders/Desafio 1/assets/js/script.js
@@ -4,9 +4,11 @@ form.addEventListener('submit', (e) => {
   e.preventDefault();
   let name = document.getElementById('name').value;
   let email = document.getElementById('email').value;
+  let phone = document.getElementById('phone').value;
   let data = {
     name,
     email,
+    phone,
   }
   let convertData = JSON.stringify(data);
 
